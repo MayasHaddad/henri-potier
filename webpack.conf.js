37b471ac49af5d9d45eae46dc241947f5bc4c04f
Mayas Haddad <mayas_91@hotmail.com>
2016-02-17 00:28:00 +0100

    add webpack dist and dev conf files

---
+++ webpack.conf.js
@@ -0,0 +1,42 @@
+/*
+ * Webpack development server configuration
+ *
+ * This file is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
+ */
+'use strict';
+var webpack = require('webpack')
+
+module.exports = {
+
+    entry: './src/main.js',
+
+    output: {
+        filename: 'dist/index.js'
+    },
+
+    eslint: {
+        configFile: '.eslintrc'
+    },
+
+    module: {
+        preLoaders: [{
+            test: /\.(js)$/,
+            exclude: /node_modules|dist/,
+            loader: 'eslint-loader'
+        }],
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.png$/, loader: "url-loader?mimetype=image/png" },
+            { test: /\.html$/, loader: "raw-loader" }
+        ]
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            Backbone: "backbone",
+            _: "underscore"
+        })
+    ]
+}
