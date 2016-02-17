9ebc25246dfbb4eaf72dfc8e2c9de5deae9ff606
Mayas Haddad <mayas_91@hotmail.com>
2016-02-17 22:53:28 +0100

    add book model spec file

---
+++ spec/models/BookSpec.js
@@ -0,0 +1,21 @@
+describe("Player", function () {
+    var Book = require('../../src/models/Book.js'),
+        book
+
+    beforeEach(function () {
+        book = new Book()
+    })
+
+    describe('Book model', function () {
+
+        describe('when instantiated', function () {
+
+            it('should exhibit attributes', function () {
+                expect(book.get('isbn')).toEqual('')
+                expect(book.get('title')).toEqual('')
+                expect(book.get('price')).toEqual(0)
+                expect(book.get('cover')).toEqual('')
+            })
+        })
+    })
+})
