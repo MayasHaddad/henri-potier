084849082f6562552633941514aed96e84a4d244
Mayas Haddad <mayas_91@hotmail.com>
2016-02-20 16:11:20 +0100

    inject jsdom windo into jquery and backbone's jquery

--- spec/helpers/MainHelper.js
+++ spec/helpers/MainHelper.js
@@ -1,3 +1,4 @@
-GLOBAL.$ = require('jquery')
+GLOBAL.$ = require('jquery')(require('jsdom').jsdom().defaultView)
 GLOBAL.Backbone = require('backbone')
+GLOBAL.Backbone.$ = $
 GLOBAL._  = require('underscore')
