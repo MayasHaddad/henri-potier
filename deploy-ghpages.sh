50334172b6b795d043b6006a3acaa4daf30a58ba
Travis-CI <travis@gpagesdeploy.com>
2016-02-22 23:16:27 +0100

    fix deploy-ghpages.sh file

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-rm -rf out || exit 0;
-mkdir out; 
-( cd out
- git init
- git config user.name "Travis-CI"
- git config user.email "travis@gpagesdeploy.com"
- cp ../dist ./dist
- cp ../index.html ./index.html
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >
-)
+
+rm -rf out || exit 0
+mkdir out
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@gpagesdeploy.com"
+cp ../dist ./dist
+cp ../index.html ./index.html
+git commit -a -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
